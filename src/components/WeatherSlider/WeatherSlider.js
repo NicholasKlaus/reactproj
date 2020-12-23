69b4bf75b84aa78ffb092177a886eeb65407ec51
Mukola <gumenukmikola@gmail.com>
2020-12-23 17:57:24 +0200

    added breakpoints 1024,768,576,320

--- src/components/WeatherSlider/WeatherSlider.js
+++ src/components/WeatherSlider/WeatherSlider.js
@@ -10,6 +10,39 @@ export const WeatherSlider = ({ children, config}) => {
     slidesToShow: 4,
     slidesToScroll: 4,
     initialSlide: 0,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2
+        }
+      },
+      {
+        breakpoint: 576,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 320,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ],
     ...config
   };
 
