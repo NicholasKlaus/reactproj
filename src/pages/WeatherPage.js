bae1b011d405a063439378fdaf68832f3b563875
NicholasKlaus <57706912+NicholasKlaus@users.noreply.github.com>
2021-01-24 14:01:18 +0200

    edit spinner and errormessage

--- src/pages/WeatherPage.js
+++ src/pages/WeatherPage.js
@@ -54,49 +54,49 @@ export const WeatherPage = () => {
             wReqFail ?
               <ErrorMessage />
             :
-            <div className='w-body'>
-              <div className="w-body__top">
-                <Row>
-                  <Col sm={6}>
-                    <div className="w-list_left_wrap">
-                      {
-                        !weatherData.length ?
-                          loading && <Spinner animation="border" variant="light" role="status" />
-                          :
-                          (
-                            <ul className="w-left_content">
-                              <li>
-                                <span className="w-data_date">{ weekDayDate } { weekDay }</span>
-                              </li>
-                              <li>
-                                <h2 className="w-data_temperature">Day {Math.round(weatherData[0].temp.day)}&deg;C</h2>
-                                <h3 className="w-data_temp-feels">Feels like { Math.round(weatherData[0].feels_like.day) }&deg;C</h3> 
-                              </li>
-                              <li>
-                                <h4 className="w-data">Humidity - { weatherData[0].humidity }%</h4> 
-                              </li>
-                              <li>
-                                <h4 className="w-data">Air Pressure - { weatherData[0].pressure }ps</h4> 
-                              </li>
-                              <li>
-                                <h4 className="w-data">Wind Speed - {weatherData[0].wind_speed }km/h</h4> 
-                              </li>
-                            </ul>
-                          )
-                      }
-                    </div>
-                  </Col>
-                  <Col sm={6}>
-                    <div className="city-name__wrap">
-                      <h1 className="w-data_city-name">{query.name}</h1>
-                    </div>
-                  </Col>
-                </Row>
+            loading ?
+              <div className="spinner-wrap">
+                <Spinner animation="border" variant="light" role="status" />
               </div>
-              <div className="w-body__bottom">
-                <CardList data={ weatherData }/>
+            :
+            (
+              <div className='w-body'>
+                <div className="w-body__top">
+                  <Row>
+                    <Col sm={6}>
+                      <div className="w-list_left_wrap">
+                        <ul className="w-left_content">
+                          <li>
+                            <span className="w-data_date">{ weekDayDate } { weekDay }</span>
+                          </li>
+                          <li>
+                            <h2 className="w-data_temperature">Day {Math.round(weatherData[0].temp.day)}&deg;C</h2>
+                            <h3 className="w-data_temp-feels">Feels like { Math.round(weatherData[0].feels_like.day) }&deg;C</h3> 
+                          </li>
+                          <li>
+                            <h4 className="w-data">Humidity - { weatherData[0].humidity }%</h4> 
+                          </li>
+                          <li>
+                            <h4 className="w-data">Air Pressure - { weatherData[0].pressure }ps</h4> 
+                          </li>
+                          <li>
+                            <h4 className="w-data">Wind Speed - {weatherData[0].wind_speed }km/h</h4> 
+                          </li>
+                        </ul>
+                      </div>
+                    </Col>
+                    <Col sm={6}>
+                      <div className="city-name__wrap">
+                        <h1 className="w-data_city-name">{query.name}</h1>
+                      </div>
+                    </Col>
+                  </Row>
+                </div>
+                <div className="w-body__bottom">
+                  <CardList data={ weatherData }/>
+                </div>
               </div>
-            </div> 
+            )
           }
         </div>
       </MainLayout>
